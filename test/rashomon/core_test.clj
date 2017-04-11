b04427e6c4eeba5bb89af1ba34959a5285129e4d
Daniel Revere <dcprevere@gmail.com>
2017-04-11 23:44:45 +0100

    Rewrite

--- test/rashomon/core_test.clj
+++ test/rashomon/core_test.clj
@@ -1,39 +1,19 @@
 (ns rashomon.core-test
-  (:require [clojure.test :refer :all]
-            [rashomon.core :refer :all]))
+  (:require [clojure.test :as t]
+            [rashomon.core :as sut]))
 
-(def rams {"AggregateCreated"
-          (fn [agg event]
-            (assoc agg
-                   :aggregate/id
-                   (:aggregate/id event)))
-          "AmountChanged"
-          (fn [agg event]
-            (assoc agg
-                   :aggregate/amount
-                   (:aggregate/amount event)))
-          "AmountDecreased"
-          (fn [agg event]
-            (update agg :aggregate/amount
-                    #(- (or % 0) %2)
-                    (:aggregate/decrease event)))})
+(t/deftest persp->fn
+  (t/testing ""
+    (t/is (= nil
+             nil))))
 
-(deftest rebuild-test
-  (testing "Can create an aggregate."
-    (is (= {:aggregate/id "32"}
-           (rebuild rams [{:event/type "AggregateCreated"
-                          :aggregate/id "32"}]))))
-
-  (testing "Can add an amount."
-    (is (= {:aggregate/id "32" :aggregate/amount 54}
-           (rebuild rams [{:event/type "AggregateCreated"
-                          :aggregate/id "32"}
-                         {:event/type "AmountChanged"
-                          :aggregate/amount 54}]))))
-
-  (testing "Decrease on unexisting amount."
-    (is (= {:aggregate/id "32" :aggregate/amount -4}
-           (rebuild rams [{:event/type "AggregateCreated"
-                          :aggregate/id "32"}
-                         {:event/type "AmountDecreased"
-                          :aggregate/decrease 4}])))))
+(t/deftest rebuild
+  (t/testing "We can rebuild a testimony from a list of events and perspectives."
+    (t/is (= 5
+             (sut/rebuild [{:rashomon.event/type :rashomon.event.type/add :num 5}]
+                          {:rashomon.event.type/add
+                           (fn [testimony event]
+                             (let [n (:num event)]
+                               (if (nil? testimony)
+                                 n
+                                 (+ n testimony))))})))))
