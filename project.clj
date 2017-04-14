747a63da2bcbff968294e5f77dae5a3adb0c7aa7
Daniel Revere <dcprevere@gmail.com>
2017-04-14 13:42:53 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject rashomon "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject rashomon "0.2.0"
+  :description "Rashomon helps you reconstruct a Clojure datastructure from a list of events."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
